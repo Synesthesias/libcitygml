b9b85aba88c238eb5cac9aa987dca8bb00255c1f
Linoal <1321932+linoal@users.noreply.github.com>
2025-03-10 21:26:13 +0900

    Covers a case where NodeType::GML_DescriptionNode is child of some attributes.
    same

--- sources/src/parser/gmlobjectparser.cpp
+++ sources/src/parser/gmlobjectparser.cpp
@@ -40,13 +40,6 @@ namespace citygml {
 
         if (node != NodeType::InvalidNode) return true;
 
-        size_t pos = node.name().find_first_of(":");
-        if (pos != std::string::npos) {
-            if (isupper(node.name().substr(pos + 1).at(0))) {// Ignore Tag start with capital letter
-                m_adeDataComingFlg = false;
-                return true;
-            }
-        }
         AttributesMap attributeSet;
         m_attributeHierarchy->push_back(attributeSet);
 
@@ -61,30 +54,15 @@ namespace citygml {
             throw std::runtime_error("Invalid call to GMLObjectElementParser::parseChildElementEndTag");
         }
 
-        if (node == NodeType::GML_DescriptionNode
-            || node == NodeType::GML_IdentifierNode
-            || node == NodeType::GML_NameNode
-            || node == NodeType::GML_DescriptionReferenceNode
-            || node == NodeType::GML_MetaDataPropertyNode) {
 
-            if (m_lastCodeSpace == "") {
-                getObject()->setAttribute(node.name(), characters);
-            } else {
-                const auto codeValue = m_factory.getCodeValue(m_lastCodeSpace, getDocumentLocation().getDocumentFilePath(), characters);
-                getObject()->setAttribute(node.name(), codeValue, detectAttributeType(codeValue));
-            }
+        if (node != NodeType::InvalidNode
+            && node != NodeType::GML_DescriptionNode
+            && node != NodeType::GML_IdentifierNode
+            && node != NodeType::GML_NameNode
+            && node != NodeType::GML_DescriptionReferenceNode
+            && node != NodeType::GML_MetaDataPropertyNode
+        ) return true;
 
-            return true;
-        }
-
-        if (node != NodeType::InvalidNode) return true;
-
-        size_t pos = node.name().find_first_of(":");
-        if (pos != std::string::npos) {
-            if (isupper(node.name().substr(pos + 1).at(0))) {// Ignore Tag start with capital letter
-                return true;
-            }
-        }
 
         if (m_skipped_tag->size() != 0) {
             if (m_skipped_tag->back() == node.name()) {
@@ -93,9 +71,8 @@ namespace citygml {
             }
         }
 
-        auto& attributesMap = m_attributeHierarchy->back();
-
         if (m_attributeHierarchy->size() == 1) { // no parent tag
+            auto& attributesMap = m_attributeHierarchy->back();
             if (attributesMap.size() == 0) { // no child tag
                 if (m_lastCodeSpace == "") {
                     getObject()->setAttribute(node.name(), characters, detectAttributeType(characters));
@@ -116,10 +93,11 @@ namespace citygml {
                     }
                     key = key + std::to_string(cnt);
                 }
-                getObject()->getAttributes()[node.name()] = attributesMap;
+                getObject()->getAttributes()[key] = attributesMap;
             }
             m_attributeHierarchy->pop_back();
-        } else { // have parent tag
+        } else if(m_attributeHierarchy->size() >= 2) { // have parent tag
+            auto& attributesMap = m_attributeHierarchy->back();
             if (attributesMap.size() == 0) { // no child tag
                 m_attributeHierarchy->pop_back();
                 auto& parent_attributesMap = m_attributeHierarchy->back();
@@ -173,13 +151,7 @@ namespace citygml {
     }
 
     bool GMLObjectElementParser::parseChildElementBothTag(const NodeType::XMLNode& node, const std::string& characters) { // Only called from UnknownElementParser::endElement
-
-        size_t pos = node.name().find_first_of(":");
-        if (pos != std::string::npos) {
-            if (isupper(node.name().substr(pos + 1).at(0))) {// Ignore Tag start with capital letter
-                return true;
-            }
-        }
+        
 
         if (m_attributeHierarchy->size() == 0) { // no parent tag
             getObject()->setAttribute(node.name(), characters, detectAttributeType(characters));
